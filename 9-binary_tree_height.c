73ec8b21d2b84938e11239537089778c8f7359d7
BruceIsBat <awoteduibrahim@gmail.com>
2023-11-03 11:59:07 -0700

    added a different rule

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,17 +3,20 @@
 /**
  * binary_tree_height - Measures the height of a binary tree.
  * @tree: Pointer to the root node of the tree to measure the height.
- * Return: The height of the tree, or 0 if the tree is NULL.
+ * Return: The height of the tree, or -1 if the tree is NULL.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
-
 	if (tree == NULL)
-		return 0;
+		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	size_t left_height = 0, right_height = 0;
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	if (tree->left)
+		left_height = binary_tree_height(tree->left) + 1;
+
+	if (tree->right)
+		right_height = binary_tree_height(tree->right) + 1;
+
+	return (left_height > right_height ? left_height : right_height);
 }
