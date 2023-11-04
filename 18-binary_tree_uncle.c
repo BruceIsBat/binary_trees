5d590213c9afe4314534a0c0bb717e9d47e0121d
BruceIsBat <awoteduibrahim@gmail.com>
2023-11-04 02:39:24 -0700

    added

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,7 +7,8 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	if (node == NULL || node->parent == NULL ||
+			node->parent->parent == NULL)
 		return (NULL);
 
 	if (node->parent == node->parent->parent->left)
@@ -16,5 +17,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node->parent == node->parent->parent->right)
 		return (node->parent->parent->left);
 
-	return NULL;
+	return (NULL);
 }
